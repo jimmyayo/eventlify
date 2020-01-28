34956eda3732143c0c1a1aab726037f70df0072e
James Yoo <jimboyoo@gmail.com>
2020-01-27 18:02:57 -0800

    add UserAccessor to get current user from HTTP

--- API/Startup.cs
+++ API/Startup.cs
@@ -81,6 +81,7 @@ namespace API
              });
          //services.TryAddSingleton<ISystemClock, SystemClock>();
          services.AddScoped<IJwtGenerator, JwtGenerator>();
+         services.AddScoped<IUserAccessor, UserAccessor>();
       }
 
       // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
