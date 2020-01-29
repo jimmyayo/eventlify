444745378e2d0bd584b83be7e1b4e480d0486e0f
James Yoo <jimboyoo@gmail.com>
2020-01-29 09:14:42 -0800

    make activity creator the event host

--- Application/Activities/Create.cs
+++ Application/Activities/Create.cs
@@ -1,9 +1,12 @@
 using System;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Application.Interfaces;
 using Domain;
 using FluentValidation;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace Application.Activities
@@ -21,41 +24,58 @@ namespace Application.Activities
          public string Venue { get; set; }
       }
 
-      public class CommandValidator: AbstractValidator<Command>
+      public class CommandValidator : AbstractValidator<Command>
       {
          public CommandValidator()
          {
-             RuleFor(x => x.Title).NotEmpty();
-             RuleFor(x => x.Description).NotEmpty();
-             RuleFor(x => x.Category).NotEmpty();
-             RuleFor(x => x.Date).NotEmpty();
-             RuleFor(x => x.City).NotEmpty();
-             RuleFor(x => x.Venue).NotEmpty();
+            RuleFor(x => x.Title).NotEmpty();
+            RuleFor(x => x.Description).NotEmpty();
+            RuleFor(x => x.Category).NotEmpty();
+            RuleFor(x => x.Date).NotEmpty();
+            RuleFor(x => x.City).NotEmpty();
+            RuleFor(x => x.Venue).NotEmpty();
          }
       }
 
       public class Handler : IRequestHandler<Command>
       {
          private readonly DataContext _context;
-         public Handler(DataContext context)
+         private readonly IUserAccessor _userAccessor;
+         public Handler(DataContext context, IUserAccessor userAccessor)
          {
+            _userAccessor = userAccessor;
             _context = context;
          }
          public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
          {
-            var activity = new Activity {
-                Id= request.Id,
-                Title = request.Title,
-                Description = request.Description,
-                Category = request.Category,
-                Date = request.Date,
-                City = request.City,
-                Venue = request.Venue
+            var activity = new Activity
+            {
+               Id = request.Id,
+               Title = request.Title,
+               Description = request.Description,
+               Category = request.Category,
+               Date = request.Date,
+               City = request.City,
+               Venue = request.Venue
             };
 
             _context.Activities.Add(activity);
-            var success = await _context.SaveChangesAsync() > 0;
 
+            var user = await _context.Users.SingleOrDefaultAsync(
+               x => x.UserName == _userAccessor.GetCurrentUsername());
+            
+            var attendee = new UserActivity 
+            {
+               AppUser = user,
+               Activity = activity,
+               IsHost = true,
+               DateJoined = DateTime.Now
+            };
+
+            _context.UserActivities.Add(attendee);
+
+            var success = await _context.SaveChangesAsync() > 0;
+            
             if (success) return Unit.Value;
 
             throw new Exception("Problem saving Activity");
