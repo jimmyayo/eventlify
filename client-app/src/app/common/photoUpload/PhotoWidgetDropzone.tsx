4f89c21d6d535fe241dc042115fc5b9cb22d2850
James Yoo <jimboyoo@gmail.com>
2020-02-06 08:21:04 -0800

    Image drag n drop, Preview image

---
+++ client-app/src/app/common/photoUpload/PhotoWidgetDropzone.tsx
@@ -0,0 +1,47 @@
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone';
+import { Icon, Header } from 'semantic-ui-react';
+
+interface IProps {
+   setFiles: (files: object[]) => void;
+}
+
+const dropzoneStyles = {
+   border: 'dashed 3px',
+   borderColor: '#eee',
+   borderRadius: '5px',
+   paddingTop: '30px',
+   textAlign: 'center' as 'center',
+   height: '200px'
+};
+
+const dropzoneActive = {
+   borderColor: 'green'
+}
+
+const PhotoWidgetDropzone: React.FC<IProps> = ({ setFiles }) => {
+   const onDrop = useCallback(acceptedFiles => {
+      // Do something with the files
+      // Note: acceptedFiles will only have 1 file in array
+      setFiles(acceptedFiles.map((file: object) => {
+         return Object.assign(file, {
+            preview: URL.createObjectURL(file)
+         })
+      }))
+   }, []);
+
+   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
+
+   return (
+      <div
+         {...getRootProps()}
+         style={isDragActive ? { ...dropzoneStyles, ...dropzoneActive } : { ...dropzoneStyles }}
+      >
+         <input {...getInputProps()} />
+         <Icon name='upload' size='huge' />
+         <Header content='Drop image here' />
+      </div>
+   )
+}
+
+export default PhotoWidgetDropzone;
