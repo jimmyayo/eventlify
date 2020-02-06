91ea01344434efa14f079509e0f56e80a4366ca0
James Yoo <jimboyoo@gmail.com>
2020-02-06 05:38:37 -0800

    Profile page layout, tab panes

--- client-app/src/features/profiles/ProfilePage.tsx
+++ client-app/src/features/profiles/ProfilePage.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Grid } from 'semantic-ui-react';
+import ProfileHeader from './ProfileHeader';
+import ProfileContent from './ProfileContent';
 
 const ProfilePage = () => {
    return (
-      <div>
-         <h1>Profile Page</h1>
-      </div>
+      <Grid>
+         <Grid.Column width={16}>
+            <ProfileHeader />
+            <ProfileContent />
+         </Grid.Column>
+      </Grid>
    )
 }
 
