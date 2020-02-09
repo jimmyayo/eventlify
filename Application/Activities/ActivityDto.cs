ed880c5e20c8f629b02ee3e676d6971c48295653
James Yoo <jimboyoo@gmail.com>
2020-02-08 17:07:18 -0800

    CommentDTO, Mapping file

--- Application/Activities/ActivityDto.cs
+++ Application/Activities/ActivityDto.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Text.Json.Serialization;
+using Application.Comments;
 
 namespace Application.Activities
 {
@@ -16,6 +17,6 @@ namespace Application.Activities
 
         [JsonPropertyName("attendees")]
         public ICollection<AttendeeDto> UserActivities { get; set; }
-
+        public ICollection<CommentDto> MyProperty { get; set; }
     }
 }
