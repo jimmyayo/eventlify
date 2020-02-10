fbc0db157b885f2f662543e356ffc2ce51b4dbd9
James Yoo <jimboyoo@gmail.com>
2020-02-10 12:13:38 -0800

    Injectable IProfileReader , follower count

--- API/Startup.cs
+++ API/Startup.cs
@@ -22,6 +22,7 @@ using Microsoft.AspNetCore.Mvc.Authorization;
 using AutoMapper;
 using Infrastructure.Photos;
 using API.SignalR;
+using Application.Profiles;
 
 namespace API
 {
@@ -113,6 +114,7 @@ namespace API
          services.AddScoped<IJwtGenerator, JwtGenerator>();
          services.AddScoped<IUserAccessor, UserAccessor>();
          services.AddScoped<IPhotoAccessor, PhotoAccessor>();
+         services.AddScoped<IProfileReader, ProfileReader>();
          services.Configure<CloudinarySettings>(Configuration.GetSection("Cloudinary"));
       }
 
--- Application/Profiles/Details.cs
+++ Application/Profiles/Details.cs
@@ -16,24 +16,14 @@ namespace Application.Profiles
 
       public class Handler : IRequestHandler<Query, Profile>
       {
-         private DataContext _context;
-         public Handler(DataContext context)
+         private readonly IProfileReader _profileReader;
+         public Handler(IProfileReader profileReader)
          {
-            _context = context;
+            _profileReader = profileReader;
          }
          public async Task<Profile> Handle(Query request, CancellationToken cancellationToken)
          {
-            var user = await _context.Users.SingleOrDefaultAsync(u =>
-                u.UserName == request.UserName);
-            
-            return new Profile
-            {
-                DisplayName = user.DisplayName,
-                UserName = user.UserName,
-                Image = user.Photos.FirstOrDefault(x => x.IsMain)?.Url ,
-                Photos = user.Photos,
-                Bio = user.Bio
-            };
+            return await _profileReader.ReadProfile(request.UserName);
          }
       }
    }
