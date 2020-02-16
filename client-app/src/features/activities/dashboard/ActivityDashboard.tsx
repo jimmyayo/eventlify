5360f1c9d5f83c5d90402246e001721ed565d1d7
James Yoo <jimboyoo@gmail.com>
2020-02-15 22:25:41 -0800

    loading placeholders

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -2,10 +2,10 @@ import React, { useContext, useEffect, useState } from 'react';
 import { Grid, Loader } from 'semantic-ui-react';
 import ActivityList from './ActivityList';
 import { observer } from 'mobx-react-lite';
-import LoadingComponent from '../../../app/layout/LoadingComponent';
 import { RootStoreContext } from '../../../app/stores/rootStore';
 import InfiniteScroll from 'react-infinite-scroller';
 import ActivityFilters from './ActivityFilters';
+import ActivityListItemPlaceholder from './ActivityListItemPlaceholder';
 
 const ActivityDashboard = () => {
 
@@ -25,20 +25,25 @@ const ActivityDashboard = () => {
    },
       [loadActivities]);
 
-   if (loadingInitial && page === 0) return <LoadingComponent content='Loading activities...' />
-
 
    return (
       <Grid>
          <Grid.Column width={10}>
-            <InfiniteScroll 
-               pageStart={0}
-               loadMore={handleGetNext}
-               hasMore={!isLoadingNext && page + 1 < totalPages }
-               initialLoad={false}
-               >
-               <ActivityList />
-            </InfiniteScroll>
+            {loadingInitial && page === 0 ?
+               <ActivityListItemPlaceholder />
+               :
+               (
+                  <InfiniteScroll
+                     pageStart={0}
+                     loadMore={handleGetNext}
+                     hasMore={!isLoadingNext && page + 1 < totalPages}
+                     initialLoad={false}
+                  >
+                     <ActivityList />
+                  </InfiniteScroll>
+               )
+            }
+
          </Grid.Column>
          <Grid.Column width={6}>
             <ActivityFilters />
